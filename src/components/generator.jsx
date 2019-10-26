5cb2eae0b6e4fb5b374b9f4211b3b9d4e0012371
Vitalii Sokolov <v.sokolov@futuro@team>
2019-10-26 17:08:05 +0300

    added react-clipboard

--- src/components/generator.jsx
+++ src/components/generator.jsx
@@ -1,43 +1,38 @@
 import React from 'react';
 import './generator.css';
-
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 class Generator extends React.Component {
   constructor(props) {
     super();
-    this.generate = this.generate.bind(this)
-    this.copy = this.copy.bind(this)
+    this.generate = this.generate.bind(this);
+    this.onCopy = this.onCopy.bind(this);
     this.passwordRef = React.createRef();
     this.popupRef = React.createRef();
     this.state = {
-      lib: '',
-      clipboard: '',
-      result: ''
+      result: '',
     }
   };
 
-  // Imported simple function
-  updateClipboard = (newClip) => {
-    navigator.clipboard.writeText(newClip).then(function() {
-    console.log('Password coppied: ', newClip);
-    });
-  };
-
-  // Function which coppies new password to the buffer
-  copy = () => {
+  // React-Copy-to-Clipboard
+  onCopy = () => {
+    // Declaring refs
     let coppied = this.passwordRef.current.innerText;
     let poppupElement = this.popupRef.current;
-    // Showing popup on 1000 ms
+    // Toogle copy
+    this.setState({... {copied: true} });
+    // Displaying popup
     poppupElement.classList.add('active');
     const hidden = () => {
       poppupElement.classList.remove('active');
     }
-    setTimeout(hidden, 1000);
-    this.setState({ clipboard:coppied });
-    this.updateClipboard(coppied);
+    // Timeout for hidding popup
+    setTimeout(hidden, 500);
+    console.log(this.state);
   };
 
   // Function which generate password
   generate = () => {
+    console.log(this.state);
     // All expected characters
     let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
     let password = [];
@@ -67,7 +62,9 @@ class Generator extends React.Component {
         </div>
         <div className="buttons">
           <button className="gen" onClick={ () => { this.generate();} } >Generate password</button>
-          <button className="copy" onClick={ () => { this.copy();} } >Copy to clipboard</button>
+          <CopyToClipboard onCopy={this.onCopy} text={this.state.result} >
+            <button className="copy" >Copy to clipboard</button>
+          </CopyToClipboard>
         </div>
         <input className="check" type="text" placeholder="Check Your Result: " />
       </div>
