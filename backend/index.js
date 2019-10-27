8cbca4aceeb2c408d383982f60dbae060a3bc809
Vitalii Sokolov <v.sokolov@futuro@team>
2019-10-27 23:58:23 +0200

    connected to app

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+// app.listen(3001, function () {
+//   console.log('Example app listening on port 3001!');
+// });
+
+
+const port = process.env.PORT || 5000;
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/', (req, res) => {
+  res.send(
+    // { express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }
+    'hello'
+  );
 });
--- src/App.js
+++ src/App.js
@@ -4,17 +4,50 @@ import logo from './vs.svg';
 import Generator from './components/generator';
 import SignIn from './components/sign-in';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="header">
-        <object className="logo" data={logo} type="image/svg+xml"></object>
-        <h3> Password Generator </h3>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then( (res) => {
+        // console.log(res);
+        // this.setState({ data: res.express });
+      })
+      .catch(err => console.log(err));
+      // console.log(this.state);
+  }
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/');
+    console.log(response);
+
+    const body = await response.body;
+    console.log(body);
+
+    // if (response.status !== 200) {
+    //   throw Error(body.message)
+    // }
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <object className="logo" data={logo} type="image/svg+xml"></object>
+          <h3> Password Generator </h3>
+        </div>
+        <SignIn />
+        <Generator image={logo}/>
       </div>
-      <SignIn />
-      <Generator image={logo}/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
