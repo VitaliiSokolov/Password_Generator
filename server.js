752c2b8e05c297c6cd194cb8bb33075ea550bf81
Vitalii Sokolov <v.sokolov@futuro@team>
2019-11-06 23:43:00 +0200

    deploy test 7

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const  NewPassword = require('./api/addNewPassword');
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   next();
 });
 // Setting up bodyParser to use json and set it to req.body
